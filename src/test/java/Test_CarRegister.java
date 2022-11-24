9de8f9419eb4376f6eed3a80ac3484e996c64da7
BurritoYunus <yunu22356@Hotmail.com>
2022-11-24 16:08:51 +0100

    Versjon 2.42
    Har lagt til en test som tester om man kan legge til flere biler med samme registreringsnummer.
    Har lagt til en test som sjekker om det bare er tilgjengelig biler på tilgjengelig biler listen.

--- src/test/java/Test_CarRegister.java
+++ src/test/java/Test_CarRegister.java
@@ -30,7 +30,7 @@ public class Test_CarRegister {
         CarRegistration car1 = new CarRegistration("PG46886", "2", "Auto", "Petrol");
         testRepository.addCar(car1);
         assertTrue(testRepository.getCarArrayList().contains(car1));
-    }    
+    }
 
     @Test
     public void remove_car_from_repository(){
@@ -41,6 +41,7 @@ public class Test_CarRegister {
         testRepository.removeCar(car1);
         assertFalse(testRepository.getCarArrayList().contains(car1));
     }
+
 
     @Test
     public void writing_and_reading_to_json_and_checking_if_still_same_class(){
@@ -67,13 +68,35 @@ public class Test_CarRegister {
                     new FileReader("test_repository.json"));
             testRepostory2 = gson.fromJson(br, CarRepository.class);
         } catch (FileNotFoundException e) {
-            System.out.println("No save file to read from. It will be made when this application closes.");
+            System.out.println("No save file to read from. Please make sure you're trying to read an existing file.");
         }catch (IOException e) {
             e.printStackTrace();
         }
         assertFalse(testRepostory2.getCarArrayList().isEmpty());
         assertSame(testRepostory2.getCarArrayList().get(0).getClass(), CarRegistration.class);
 
+    }
+
+    @Test
+    public void repository_does_not_accept_identical_registration_numbers(){
+        CarRepository testRepository = new CarRepository("test_repository.json");
+        CarRegistration car1 = new CarRegistration("PG46886", "2", "Auto", "Petrol");
+        CarRegistration car2 = new CarRegistration("PG46886", "6", "Manual", "Diesel");
+        testRepository.addCar(car1);
+        testRepository.addCar(car2);
+        assertFalse(testRepository.getCarArrayList().contains(car2));
+    }
+
+    @Test
+    public void available_list_only_contains_available_cars(){
+        CarRepository testRepository = new CarRepository("test_repository.json");
+        CarRegistration car1 = new CarRegistration("PG46886", "2", "Auto", "Petrol");
+        CarRegistration car2 = new CarRegistration("65GHDG3", "6", "Manual", "Diesel");
+        car1.setRented(true);
+        testRepository.addCar(car1);
+        testRepository.addCar(car2);
+        assertFalse(testRepository.getAllAvailableCars().contains(car1));
+        assertTrue(testRepository.getAllAvailableCars().contains(car2));
     }
 
 }
