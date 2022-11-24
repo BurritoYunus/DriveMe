dbc961350ef9cb9711361c4468709914f5b4787f
BurritoYunus <yunu22356@Hotmail.com>
2022-11-24 13:57:57 +0100

    Versjon 2.3
    Lagt til test om man kan rente en bil.

--- src/test/java/Test_CarRegister.java
+++ src/test/java/Test_CarRegister.java
@@ -7,8 +7,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Test_CarRegister {
     @Test
-    public void creating_car_without_rented_parameter(){
+    public void creating_car_without_rented_parameter_is_not_rented_on_default(){
         CarRegistration notRentedCar = new CarRegistration("EG5345", "4", "Manual", "Diesel");
         assertFalse(notRentedCar.isRented());
+    }
+
+    @Test
+    public void creating_car_and_renting_it_out(){
+        CarRegistration rentedCar = new CarRegistration("PG46886", "2", "Auto", "Petrol");
+        rentedCar.setRented(true);
+        assertTrue(rentedCar.isRented());
     }
 }
